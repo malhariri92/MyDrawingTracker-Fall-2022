46411ec59e3d7c53ce88e32333a6370973a09133
Mutasem-Alhariri <alhari01@email.franklin.edu>
2022-09-23 20:00:03 -0400

    #1 Create Group Admin Account
    Created a view, a view model, and two action methods in the home controller.

--- MDT/Controllers/HomeController.cs
+++ MDT/Controllers/HomeController.cs
@@ -239,5 +239,81 @@ namespace MDT.Controllers
 
             return RedirectToAction("Index", "Home");
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult CreateAdmin()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult CreateAdmin(AdminUserVM vm)
+        {
+            using (var db = new DbEntities())
+            {
+                var user = db.Users.Where(u => u.EmailAddress.Equals(vm.EmailAddress)).FirstOrDefault();
+                if(user != null)
+                {
+                    ModelState.AddModelError("EmailAddress","Email addrress is already in use.");
+                }
+
+                var group = db.Groups.Where(g => g.GroupName.Equals(vm.GroupName)).FirstOrDefault();
+                if (group != null)
+                {
+                    ModelState.AddModelError("GroupName", "A group with this name already exists.");
+                }
+                else
+                {
+                    // Create a new group
+                    group = new Group()
+                    {
+                        GroupName = vm.GroupName,
+                        IsActive = true,
+                        IsPrimary = true,
+                        JoinConfirmationRequired = true,
+                    };
+
+                    db.Groups.Add(group);
+                    db.SaveChanges();
+                    group = db.Groups.Where(g => g.GroupName.Equals(vm.GroupName)).FirstOrDefault();
+
+                    // Create a new user
+                    user = new User()
+                    {
+                        UserName = vm.UserName,
+                        EmailAddress = vm.EmailAddress,
+                        CurrentGroupId = group.GroupId,
+                        IsActive = true,
+                        IsVerified = false,
+                    };
+                    db.Users.Add(user);
+                    db.SaveChanges();
+
+                    //Hash the password and add to the newly created user.
+                    user = db.Users.Where(u => u.EmailAddress.Equals(vm.EmailAddress)).FirstOrDefault();
+                    PasswordManager.SetNewHash(user.UserId, vm.Password);
+
+                    //Create the admin group user.
+                    var groupUser = new GroupUser()
+                    {
+                        GroupId = group.GroupId,
+                        UserId = user.UserId,
+                        IsAdmin = true,
+                    };
+                    db.GroupUsers.Add(groupUser);
+                    db.SaveChanges();
+
+                    ViewBag.SuccessMessage = "Your account has been created successfully!";
+                    ModelState.Clear();
+                    return View();
+
+                }
+
+            }
+                
+            return View();
+        }
     }
 }
