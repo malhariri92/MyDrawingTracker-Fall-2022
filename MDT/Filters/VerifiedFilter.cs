58872173d157a217740d84bba16c303357348757
Stephen Gasperowich <gasper03@email.franklin.edu>
2022-09-25 14:29:37 -0400

    Adding merges

---
+++ MDT/Filters/VerifiedFilter.cs
@@ -0,0 +1,24 @@
+﻿using MDT.Models.DTO;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MDT.Filters
+{
+    public class VerifiedFilter : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            UserDTO user = (UserDTO)httpContext.Session["User"];
+            GroupDTO group = (GroupDTO)httpContext.Session["Group"];
+            httpContext.Session["RedirectUrl"] = httpContext.Request.RawUrl;
+            return user.IsVerified && (group.IsApproved ?? false);
+        }
+
+        
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            filterContext.Result = new RedirectResult("~/Home/Index");
+        }
+    }
+}
---
+++ MDT/Models/DTO/LoginDTO.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MDT.Models.DTO
+{
+    /// <summary>
+    /// Login in object for user account.
+    /// </summary>
+    public class LoginDTO
+    {
+        [Display(Name = "Email Address")]
+        [Required(ErrorMessage = "{0} is required")]
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Password - case sensitive. Will be encrypted and compared with Hash in the Users table.
+        /// </summary>
+        [Display(Name = "Password")]
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// Result of user login attempt
+        /// </summary>
+        public PasswordManager.Result LoginResult { get; set; }
+
+        /// <summary>
+        /// The User object if one exists with credentials that match those provided.
+        /// </summary>
+        public User User { get; set; }
+
+        /// <summary>
+        /// The number of failed login attempts for the user.
+        /// </summary>
+        public int FailedCount { get; set; }
+
+        /// <summary>
+        /// The timestamp of the user's first failed attempt to log in.  
+        /// </summary>
+        public DateTime? FirstFailed { get; set; }
+
+        /// <summary>
+        /// User will be locked for 30 minutes if they have more than 3 failed login attempts within 15 minutes.
+        /// </summary>
+        public bool UserLocked { get; set; }
+
+
+    }
+}
