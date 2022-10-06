a42c49df0006142d267f6b6ab7aaad150cd9ffd6
TaleGuunner <66076434+TaleGuunner@users.noreply.github.com>
2022-10-06 17:27:11 -0400

    Implemented methods to view the AddDraw page and take form requests.

--- MDT/Controllers/DrawController.cs
+++ MDT/Controllers/DrawController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using MDT.Models;
+using MDT.Models.DTO;
+using MDT.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MDT.Controllers
 {
@@ -10,6 +15,39 @@ namespace MDT.Controllers
     {
         public ActionResult Index()
         {
+            return View();
+        }
+
+        public ActionResult AddDraw()
+        {
+            GroupDTO group = (GroupDTO)Session["group"];
+            UserDTO user = (UserDTO)Session["user"];
+
+            Session["past"] = false;
+     
+            DrawVM vm = new DrawVM();
+
+            List<DrawType> test = db.DrawTypes.ToList();
+
+            foreach (DrawType dT in test)
+            {
+                vm.DrawTypes.Add(dT.DrawTypeId.ToString(), dT);                
+            }
+            
+            vm.EndDate = DateTime.Now;
+
+            return View(vm);
+        }
+
+        public ActionResult AddNewDraw(DrawVM vm)
+        {
+            Session["past"] = false;
+
+            if (vm.EndDate < DateTime.Now)
+            {
+                Session["past"] = true;
+                return PartialView("AddDraw", vm);
+            }
             return View();
         }
     }
