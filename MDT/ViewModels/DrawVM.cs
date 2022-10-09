99c21093f78ab76149f67ea2b6ae4853e2bdb12c
TaleGuunner <66076434+TaleGuunner@users.noreply.github.com>
2022-10-09 19:31:00 -0400

    Specified formatting for EndDate

--- MDT/ViewModels/DrawVM.cs
+++ MDT/ViewModels/DrawVM.cs
@@ -3,6 +3,8 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Web.Mvc;
 
 namespace MDT.ViewModels
 {
@@ -14,14 +16,15 @@ namespace MDT.ViewModels
         public DateTime? StartDate { get; set; }
 
         [Display(Name = "End Date")]
-        [Required]
+        [Required(ErrorMessage = "{0} is required.")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:MM/dd/yyyy}")]
         public DateTime EndDate { get; set; }
 
         [Display(Name = "Results")]
         public string Results { get; set; }
 
         [Display(Name ="Game")]
-        [Required]
+        [Required(ErrorMessage = "{0} is required.")]
         public int DrawTypeId { get; set; }
         public string DrawTypeName { get; set; }
 
@@ -40,7 +43,6 @@ namespace MDT.ViewModels
                 Results = d.Results;
                 DrawTypeId = d.DrawTypeId;
                 DrawTypeName = d.DrawType.DrawTypeName;
-
                 Entries = d.DrawEntries.Select(e => new EntryVM(e)).ToList();
             }
         }
