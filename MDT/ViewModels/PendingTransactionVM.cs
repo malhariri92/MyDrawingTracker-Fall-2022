481560bb5553cb7175948202eba896dd277ed351
KyleBoldman <boldma03@email.franklin.edu>
2022-10-23 17:34:40 -0400

    Validate User Reported Transaction
    Allows a group admin to look at user reported transactions and approve them.

---
+++ MDT/ViewModels/PendingTransactionVM.cs
@@ -0,0 +1,44 @@
+﻿using MDT.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+using MDT;
+
+namespace MDT.ViewModels
+{
+    public class PendingTransactionVM
+    {
+        public int PendingTransactionId { get; set; }
+        public int TransactionTypeId { get; set; }
+        public string TransactionTypeName { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+        public int? DrawId { get; set; }
+        public int SourceLedger { get; set; }
+        public string SourceLedgerName { get; set; }
+        public int DestinationLedger { get; set; }
+        public string DestinationLedgerName { get; set; }
+
+        public PendingTransactionVM(PendingTransaction transaction = null)
+        {
+            if (transaction != null)
+            {
+                PendingTransactionId = transaction.PendingTransactionId;
+                TransactionTypeId = transaction.TransactionTypeId;
+                TransactionTypeName = transaction.TransactionType.TypeName;
+                UserId = transaction.UserId;
+                UserName = transaction.User.UserName;
+                Amount = transaction.Amount;
+                TransactionDateTime = transaction.TransactionDateTime;
+                DrawId = transaction.DrawId;
+                SourceLedger = transaction.SourceLedger;
+                SourceLedgerName = transaction.FromLedger.LedgerName;
+                DestinationLedger = transaction.DestinationLedger;
+                DestinationLedgerName = transaction.ToLedger.LedgerName;
+            }
+        }
+    }
+}
