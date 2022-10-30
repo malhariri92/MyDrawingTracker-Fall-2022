63ff55af0c29b7af751a0d2815c149f07b29816f
KyleBoldman <boldma03@email.franklin.edu>
2022-10-30 19:40:23 -0400

    UI-DrawingEntities
    Allows the admin to see a list of users who have entered into a drawing, and gives the option to open a removal form for those entries.

---
+++ MDT/ViewModels/RemoveEntriesVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MDT.ViewModels
+{
+    public class RemoveEntriesVM : Controller
+    {
+        public int UserId { get; set; }
+        public int DrawId { get; set; }
+
+        [Display(Name = "Number to Remove")]
+        public int RemovedEntries { get; set; }
+    }
+}
---
+++ MDT/ViewModels/UserDrawEntriesVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MDT.ViewModels
+{
+    public class UserDrawEntriesVM 
+    {
+        public int DrawId { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public int NumEntries { get; set; }
+
+        public UserDrawEntriesVM (int DrawId, int UserId, string UserName, int NumEntries)
+        {
+            this.DrawId = DrawId;
+            this.UserId = UserId;
+            this.UserName = UserName;
+            this.NumEntries = NumEntries;
+        }
+    }
+}
